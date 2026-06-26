9dfbfda545ee14c8c49d6e539841b647fa9e8bab
gangaram Padolkar <gangaram.padolkar@gmail.com>
2026-06-26 23:55:15 +0530

    file updated with cred add

--- main.tf
+++ main.tf
@@ -1,23 +1,50 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1" # Replace with your desired AWS region
 }
 
-resource "aws_instance" "demo-ec2" {
-    ami = "ami-01a00762f46d584a1" # Replace with a valid AMI ID for your region
-    instance_type = "t2.micro"
-    # key_name = "my-key-pair" # Replace with your key pair name
-    tags = {
-        Name = "DemoEC2Instance"
-    }
-  
-}
 resource "random_string" "bucket_suffix" {
-    length  = 8
-    special = false
+  length  = 8
+  special = false
 }
-resource "aws_s3_bucket" "demo-s3-bucket" {
-    bucket = "my-demo-s3-bucket-dgdhjggjhggjgjgjhg" # Replace with a unique bucket name    
-    tags = {
-        Name = "DemoS3Bucket"
+
+resource "aws_instance" "demo_ec2" {
+  ami           = "ami-01a00762f46d584a1" # Replace with a valid AMI
+  instance_type = "t2.micro"
+
+
+  tags = {
+    Name = "DemoEC2Instance"
+  }
+}
+
+resource "aws_s3_bucket" "demo_s3_bucket" {
+  bucket = "my-demo-s3-bucket-${random_string.bucket_suffix.result}"
+
+  tags = {
+    Name = "DemoS3Bucket"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "demo_s3_encryption" {
+  bucket = aws_s3_bucket.demo_s3_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
+  }
 }
